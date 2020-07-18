7931b92ed8cd1209be55369f5548be28e57e2cb5
vinayreddy9542 <reddyv399@gmail.com>
2020-07-18 17:57:12 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,5 +1,6 @@
 public static boolean palindrome(String str){
 	    int length = str.length();
+	String rev="";
         for(int i=length-1;i>=0;i--)
             rev = rev + str.charAt(i);
         if(str.equals(rev))
@@ -7,3 +8,14 @@ public static boolean palindrome(String str){
         else
         return false;
 	}
+
+/*
+this can be done by using two variables at 0 and other at strlen-1
+if(both match){
+	i++;
+	j--;
+}
+else{
+	return false;
+}
+*/
